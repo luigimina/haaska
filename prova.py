d5e27db36cfedb843209a54a0b80ea8c044053f1
Luigi Mina <luigi.mina@gmail.com>
2020-05-11 01:32:47 +0200

    Prova di chiamata a server locale

---
+++ prova.py
@@ -0,0 +1,28 @@
+from haaska import HomeAssistant, Configuration, event_handler
+import json
+
+event = {
+  "directive": {
+    "header": {
+      "namespace": "Alexa.Discovery",
+      "name": "Discover",
+      "payloadVersion": "3",
+      "messageId": "1bd5d003-31b9-476f-ad03-71d471922820"
+    },
+    "payload": {
+      "scope": {
+        "type": "BearerToken"
+      }
+    }
+  }
+}
+
+contest= { }
+
+event_handler(event,contest)
+
+with open('event.json', 'w') as outfile1:
+    json.dump(event, outfile1, indent=4)
+
+with open('contest.json', 'w') as outfile2:
+    json.dump(contest, outfile2, indent=4)    
